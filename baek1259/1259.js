56c4827a757fc7162a6d8cfbeb91e70319bb695a
gilmujjang <alsanrlf@naver.com>
2020-12-29 22:27:49 +0900

    맞는데 자꾸 틀리데

--- baek1259/1259.js
+++ baek1259/1259.js
@@ -7,34 +7,30 @@ const input = [
 '12421',
 '12345654321',
 '1234553421',
+'3214894153156445614561231',
+'12345678987654321',
 '0'
 ]
 
-// for(i=0; i<input.length-1; i++){
-//   let q = input[i];
-//   let p = q.length;
-//   let bo = true;
-//   let j = 0;
-
-//   while(j<p/2){
-//     if(q[j]!=q[p-1-j]){
-//       bo = false;
-//       break;
-//     }
-//     j++;
-//   }
-//   if(bo){
-//     console.log("yes");
-//   } else {
-//     console.log("no");
-//   }
-// }
-
-// 맞는데 왜 틀리다는겨;;
 for(i=0; i<input.length-1; i++){
-  if(input[i]==input[i].split("").reverse().join("")){
+  let q = input[i].split('');
+  let p = q.length;
+  let bo = true;
+  let j = 0;
+
+  while(j<p/2){
+    if(q[j]!=q[p-1-j]){
+      bo = false;
+      break;
+    }
+    j++;
+  }
+  if(bo){
     console.log("yes");
   } else {
     console.log("no");
   }
 }
+
+// 맞는데 왜 틀리다는겨;;
+
