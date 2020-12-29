fbf5ffa2de06a2f8edd0a947b4c14589f584a523
gilmujjang <alsanrlf@naver.com>
2020-12-30 02:16:59 +0900

    python으로 좀품

--- baek11866/11866.py
+++ baek11866/11866.py
@@ -6,14 +6,14 @@ k = 2
 s = deque([])
 for i in range(1, n + 1):
     s.append(i)
-print('<', end='')
+print('<', end="")
 while s:
     for i in range(k - 1):
         s.append(s[0])
         s.popleft()
-    print(s.popleft(), end='')
+    print(s.popleft(), end="")
     if s:
-        print(', ', end='')
+        print(', ', end="")
 print('>')
 
 """
---
+++ baek1966/1966.py
@@ -0,0 +1,8 @@
+a = "3\n1 0\n5\n4 2\n1 2 3 4\n6 0\n1 1 9 1 1 1"
+b = a.split()
+c = list(map(int,b))
+
+print(c)
+
+for _ in range (18):
+  n,m = list(map,int(a.split(" ")))
