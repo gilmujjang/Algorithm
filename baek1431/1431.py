e54b7634422321619fdf64253cc4434ee2cc02ca
gilmujjang <alsanrlf@naver.com>
2021-01-01 20:05:13 +0900

    1431

---
+++ baek1431/1431.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline()
+
+n = int(input())
+guitar = []
+for _ in range(n):
+  serial = input()
+  sum_number = 0
+  for i in serial:
+    if i.isdigit():
+      sum_number += int(i)
+  guitar.append((serial, sum_number))
+ 
+result = sorted(
+  guitar, key=lambda x : (len(x[0]), x[1], x[0])
+)
+
+for s in result:
+  print(s[0])
+
