909a17f396bc75c6617b9a1c3eb531fb68778039
gilmujjang <alsanrlf@naver.com>
2021-01-06 17:52:17 +0900

    2467 포기

---
+++ baek2467/2467.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+num = int(stdin.readline())
+
+li = list(map(int, stdin.readline().split()))
+
+li.sort()
+
+left = 0
+right = num-1
+
+while left<=right:
+  mid = (right+left)//2
+  ans = 
+
+print(ans)
