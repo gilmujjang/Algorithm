219c2875d0e68da02c91d88c718d6502fdfb466f
gilmujjang <alsanrlf@naver.com>
2021-01-08 20:22:27 +0900

    마스터

--- baek1260/1260.py
+++ baek1260/1260.py
@@ -1,29 +1,42 @@
-#a,b,n = map(int,stdin.readline().split())
+# from sys import stdin
+# a,b,n = map(int,stdin.readline().split())
 
-a = 4
+import copy
+
+a = 5
 b = 5
-n = 1
+n = 3
 
-py = ["1 2","1 3","1 4","2 4","3 4"]
+py = ["5 4","5 2","1 2","3 4","3 1"]
 
     #   1 2 3 4 
     # 1 F T T T
     # 2 F F F T
     # 3 F F F F 
-    # 4 F F F F   => 이런식으로 표를 만들고 for문 돌려
+    # 4 F F F F 
+
+
+    #   1 2 3 4 5
+    # 1 F T T F F
+    # 2 T F F F T
+    # 3 T F F T F
+    # 4 F F T F T
+    # 5 F T F F T
 
 node = [[False for col in range(a)] for row in range(a)]
-
+node1 = [[False for col in range(a)] for row in range(a)]
 for i in range (b):
-    #x,y = map(int,append(stdin.readline().split()))
+    #x,y = map(int,stdin.readline().split())
     x,y = map(int,py[i].split())
     node[x-1][y-1] = True
     node[y-1][x-1] = True
+    node1[x-1][y-1] = True
+    node1[y-1][x-1] = True
 
 visit_dfs = []
 visit_bfs = []
 
-print(node)
+
 def dfs(n):
     visit_dfs.append(n)
     for i in range (a):
@@ -33,11 +46,21 @@ def dfs(n):
                 node[i][n-1] = False
                 dfs(i+1)   
 
-
-# def bfs():
-#     visit_bfs.append(n)
-
+def bfs(n):
+    golist = [n]
+    while golist:
+        start = golist.pop(0)
+        visit_bfs.append(start)
+        for i in range (a):
+            if node1[start-1][i] == True:
+                if i+1 not in visit_bfs:
+                    if i+1 not in golist:
+                        node1[start-1][i] = False
+                        node1[i][start-1] = False
+                        golist.append(i+1)
+                    
 
 dfs(n)
-
-print(visit_dfs)
+bfs(n)
+print(" ".join(map(str,visit_dfs)))
+print(" ".join(map(str,visit_bfs)))
