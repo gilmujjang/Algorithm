369adf3436ca24c8ef1ef0b9d43f48f7fe7f0f4b
gilmujjang <alsanrlf@naver.com>
2021-01-19 21:20:05 +0900

    1197

---
+++ baek1197/1197.py
@@ -0,0 +1,33 @@
+from sys import stdin
+n,m = map(int, stdin.readline().split())
+arr = []
+for i in range(m):
+  arr.append(list(map(int, li[i].split())))
+arr = sorted(arr, key=lambda k: k[2])
+parent = [i for i in range(0, n+1)]
+ans = 0
+node = 0
+
+def find(x):
+  if x==parent[x]:
+    return x
+  parent[x] = find(parent[x])
+  return parent[x]
+
+def union(x,y):
+  x,y = find(x),find(y)
+  if x != y:
+    parent[y] = x
+
+for i in arr:
+  start, end, weight = i
+  if find(start) == find(end):
+    continue
+  else:
+    ans += weight
+    union(start,end)
+    node += 1
+  if node == n-1:
+    break
+
+print(ans)
