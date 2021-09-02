9b62349289514674f84adcc094b058f02511b4c3
gilmujjang <alsanrlf@naver.com>
2021-09-02 16:45:51 +0900

    10845 포기

--- baekjoon/10845.js
+++ baekjoon/10845.js
@@ -1,17 +1,9 @@
-// let fs = require('fs');
-// let inpu = fs.readFileSync('/dev/stdin').toString().split('\n');
-// let input = []
-// for(i=0; i<inpu.length; i++){
-//     let a = inpu[i].toString().split(' ')
-//     for(j=0; j<a.length; j++){
-//         input.push(a[j])
-//     }
-// }
+// 이게 도대체 왜 틀린건지 모르겠네
 
 const input = [
   '15',
-  'push', '1',
-  'push', '2',
+  'push 1',
+  'push 2',
   'front',
   'back',
   'size',
@@ -22,52 +14,56 @@ const input = [
   'size',
   'empty',
   'pop',
-  'push', '3',
+  'push 3',
   'empty',
   'front'
 ]
 let num = [];
-let s = 0;
-
 
 for(i=1; i<input.length; i++){
-  let now = input[i];
-  let next = input[i+1];
-  if(input[i]=='push'){
-    num.push(next);
-    s = s + 1;
-  }
-  if(now=='pop'){
-    if(s>0){
-      console.log(num[0]);
-      num.splice(0,1);
-      s = s - 1;
-    } else {
-      console.log(-1)
-    }
-  }
-  if(now=='size'){
-    console.log(s);
-  }
-  if(now=='empty'){
-    if(s>0){
-      console.log(0)
-    } else {
-      console.log(1)
-    }
-  }
-  if(now=='front'){
-    if(s>0){
-      console.log(num[0]);
-    } else {
-      console.log(-1)
-    }
-  }
-  if(now=='back'){
-    if(s>0){
-      console.log(num[s-1]);
-    } else {
-      console.log(-1);
-    }
+  const now = input[i];
+  const s = num.length;
+  switch(now){
+    case 'pop':
+      if(s>0){
+        console.log(num[0]);
+        num.splice(0,1);
+      } else {
+        console.log(-1)
+      }
+      break;
+
+    case 'size':
+      console.log(s);
+      break;
+
+    case 'empty':
+      if(s>0){
+        console.log(0)
+      } else {
+        console.log(1)
+      }
+      break;
+
+    case 'front':
+      if(s>0){
+        console.log(num[0]);
+      } else {
+        console.log(-1)
+      }
+      break;
+
+    case 'back':
+      if(s>0){
+        console.log(num[s-1]);
+      } else {
+        console.log(-1);
+      }
+      break;
+
+    default:
+      const [n, ne] = now.split(' ')
+      num.push(ne);
+      break;
   }
 }
