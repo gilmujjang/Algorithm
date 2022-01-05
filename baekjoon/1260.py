071d591cdd240c3663198c10e5e96f5d901a14ab
gilmujjang <alsanrlf@naver.com>
2022-01-05 15:08:46 +0900

    [FIX] 1260 변수명 수정

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,35 +1,21 @@
-# from sys import stdin
-# a,b,n = map(int,stdin.readline().split())
+from sys import stdin
+a, b, n = map(int, stdin.readline().split())
 
-a = 5
-b = 5
-n = 3
+# a = 5
+# b = 5
+# n = 3
 
-py = ["5 4","5 2","1 2","3 4","3 1"]
+# py = ["5 4", "5 2", "1 2", "3 4", "3 1"]
 
-    #   1 2 3 4 
-    # 1 F T T T
-    # 2 F F F T
-    # 3 F F F F 
-    # 4 F F F F 
-
-
-    #   1 2 3 4 5
-    # 1 F T T F F
-    # 2 T F F F T
-    # 3 T F F T F
-    # 4 F F T F T
-    # 5 F T F F T
-
-node = [[False for col in range(a)] for row in range(a)]
-node1 = [[False for col in range(a)] for row in range(a)]
-for i in range (b):
+dfs_node = [[False for _ in range(a)] for _ in range(a)]
+bfs_node = [[False for _ in range(a)] for _ in range(a)]
+for i in range(b):
     #x,y = map(int,stdin.readline().split())
-    x,y = map(int,py[i].split())
-    node[x-1][y-1] = True
-    node[y-1][x-1] = True
-    node1[x-1][y-1] = True
-    node1[y-1][x-1] = True
+    x, y = map(int, py[i].split())
+    dfs_node[x - 1][y - 1] = True
+    dfs_node[y - 1][x - 1] = True
+    bfs_node[x - 1][y - 1] = True
+    bfs_node[y - 1][x - 1] = True
 
 visit_dfs = []
 visit_bfs = []
@@ -37,28 +23,29 @@ visit_bfs = []
 
 def dfs(n):
     visit_dfs.append(n)
-    for i in range (a):
-        if node[n-1][i] == True:
-            if i+1 not in visit_dfs:
-                node[n-1][i] = False
-                node[i][n-1] = False
-                dfs(i+1)   
+    for i in range(a):
+        if dfs_node[n - 1][i] == True:
+            if i + 1 not in visit_dfs:
+                dfs_node[n - 1][i] = False
+                dfs_node[i][n - 1] = False
+                dfs(i + 1)
+
 
 def bfs(n):
     golist = [n]
     while golist:
         start = golist.pop(0)
         visit_bfs.append(start)
-        for i in range (a):
-            if node1[start-1][i] == True:
-                if i+1 not in visit_bfs:
-                    if i+1 not in golist:
-                        node1[start-1][i] = False
-                        node1[i][start-1] = False
-                        golist.append(i+1)
-                    
+        for i in range(a):
+            if bfs_node[start - 1][i] == True:
+                if i + 1 not in visit_bfs:
+                    if i + 1 not in golist:
+                        bfs_node[start - 1][i] = False
+                        bfs_node[i][start - 1] = False
+                        golist.append(i + 1)
+
 
 dfs(n)
 bfs(n)
-print(" ".join(map(str,visit_dfs)))
-print(" ".join(map(str,visit_bfs)))
+print(" ".join(map(str, visit_dfs)))
+print(" ".join(map(str, visit_bfs)))
